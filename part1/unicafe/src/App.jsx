66a4cc3e50f2fe440e13a2c8be1a570cf3e0094b
Ottis1 <otto.kuusela@outlook.com>
2023-11-30 12:12:02 +0100

    Add exercise 1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+const App = () => {
+    const [goodCount, setGoodCount] = useState(0)
+    const [neutralCount, setNeutralCount] = useState(0)
+    const [badCount, setBadCount] = useState(0)
+
+    const clickHandler = (counter) => {
+        switch(counter) {
+            case "good":
+                return () => setGoodCount(goodCount + 1)
+            case "neutral":
+                return () => setNeutralCount(neutralCount + 1)
+            case "bad":
+                return () => setBadCount(badCount + 1)
+            default:
+                throw Error(`${counter} is not a recognised counter`)
+        }
+    }
+
+    return (
+        <div>
+            <h1>Give Feedback</h1>
+            <Button text="good" onClick={clickHandler("good")} />
+            <Button text="neutral" onClick={clickHandler("neutral")} />
+            <Button text="bad" onClick={clickHandler("bad")} />
+            <h2>Statistics</h2>
+            <p>good {goodCount}</p>
+            <p>neutral {neutralCount}</p>
+            <p>bad {badCount}</p>
+        </div>
+    )
+}
+
+const Button = ({ text, onClick }) =>  <button onClick={onClick}>{text}</button>
+
+export default App
